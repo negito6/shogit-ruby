22a903d2ca42346310e0fcdd8066c28946481005
toru.yagi <toru.yagi@crowdworks.co.jp>
2016-09-22 23:15:44 +0900

    Add initialization

---
+++ init.rb
@@ -0,0 +1,5 @@
+require './shogit'
+
+git = Shogit::Git.new
+Shogit::File.new.touch
+git.add_and_commit(Shogit::File.new, "init")
--- shogit.rb
+++ shogit.rb
@@ -1,3 +1,5 @@
+require 'rugged'
+
 module Shogit
   module Console
     WORDS = {
@@ -43,6 +45,72 @@ module Shogit
 
     def method_missing(method, *args)
       convert(method) || super
+    end
+  end
+
+  class Git
+    def initialize
+      path = './data/hirate'
+      Rugged::Repository.init_at(path)
+      @repo = Rugged::Repository.discover(path)
+    end
+
+    def repo
+      @repo
+    end
+
+    def git_user
+      { :email => "testuser@github.com", :name => 'Test Author' }
+    end
+
+    def add_and_commit(file, message)
+      oid = repo.write(file.content, :blob)
+      index = repo.index
+      index.read_tree(repo.head.target.tree) rescue nil
+      index.add(:path => file.relative_path, :oid => oid, :mode => 0100644)
+      index.write
+      commit(index, message)
+    end
+
+    def commit(index, message)
+      options = {}
+      options[:tree] = index.write_tree(repo)
+      options[:author]    = git_user.merge(:time => Time.now)
+      options[:committer] = git_user.merge(:time => Time.now)
+      options[:message] ||= message
+      options[:parents] = repo.empty? ? [] : [ repo.head.target ].compact
+      options[:update_ref] = 'HEAD'
+
+      Rugged::Commit.create(repo, options)
+    end
+  end
+
+  class File
+    def initialize
+      @repo = Git.new.repo
+    end
+
+    def relative_path
+      'kifu.txt'
+    end
+
+    def repo
+      @repo
+    end
+
+    def touch
+      ::File.open(path, "w") do |f|
+      end
+    end
+
+    def path
+      ::File.join(repo.workdir, relative_path)
+    end
+
+    def content
+      ::File.open(path, "r") do |f|
+        return f.read
+      end
     end
   end
 end
